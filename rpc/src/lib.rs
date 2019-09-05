949db307e2245232168273904a34464aafdfffa3
kilpatty <seanpkilgarriff@gmail.com>
2019-09-05 17:36:15 -0500

    lib: make a standard Result type

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -2,5 +2,7 @@ pub mod client;
 pub mod error;
 pub mod responses;
 
-pub use client::RPCClient;
+pub use client::HandshakeRpcClient;
 pub use error::Error;
+
+pub type Result<T> = std::result::Result<T, Error>;
