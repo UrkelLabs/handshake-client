fc8fc89b3565ddc628599de79a8f9522f9ebf300
kilpatty <seanpkilgarriff@gmail.com>
2019-09-11 14:23:10 -0500

    tests: finalize mempool tests, still need to finish ancestor tests, but need a more dynamic testing framework for those

--- rpc/tests/client_mempool_test.rs
+++ rpc/tests/client_mempool_test.rs
@@ -1,73 +1,124 @@
 mod common;
 
 use common::setup;
+use common::get_txid;
 
-#[test]
-fn test_get_mempool_info() {
+#[runtime::test]
+async fn test_get_mempool_info() {
     let client = setup();
 
-    let info = client.get_mempool_info();
+    let info = client.get_mempool_info().await;
 
     assert!(info.is_ok());
 }
 
-// #[test]
-// fn test_getmempoolancestors() {
+//@todo we need a way of creating a new tx, and then checking it in the mempool. Disabling for now.
+// #[runtime::test]
+// async fn test_get_mempool_ancestors() {
+//     let client = setup();
+
+//     let ancestors = client.get_mempool_ancestors(&tx_hash).await;
+
+//     assert!(ancestors.is_ok());
 
 // }
-//
-//TODO Finish tests that require a txhash in the mempool.
-//
-#[test]
-fn test_get_raw_mempool() {
+
+// #[runtime::test]
+// async fn test_get_mempool_ancestors_verbose() {
+//     let client = setup();
+
+//     let ancestors = client.get_mempool_ancestors_verbose(&tx_hash).await;
+
+//     assert!(ancestors.is_ok());
+
+// }
+
+// #[runtime::test]
+// async fn test_get_mempool_descendants() {
+//     let client = setup();
+
+//     let ancestors = client.get_mempool_descendants(&tx_hash).await;
+
+//     assert!(ancestors.is_ok());
+// }
+
+// #[runtime::test]
+// async fn test_get_mempool_descendants_verbose() {
+//     let client = setup();
+
+//     let ancestors = client.get_mempool_ancestors_verbose(&tx_hash).await;
+
+//     assert!(ancestors.is_ok());
+// }
+
+// #[runtime::test]
+// async fn test_get_mempool_entry() {
+//     let client = setup();
+
+//     let ancestors = client.get_mempool_ancestors(&tx_hash).await;
+
+//     assert!(ancestors.is_ok());
+
+// }
+
+#[runtime::test]
+async fn test_get_raw_mempool() {
     let client = common::setup();
 
-    let mempool = client.get_raw_mempool();
+    let mempool = client.get_raw_mempool().await;
 
     assert!(mempool.is_ok());
 }
 
-#[test]
-fn test_getrawmempool_verbose() {
+#[runtime::test]
+async fn test_get_raw_mempool_verbose() {
     let client = common::setup();
 
-    let mempool = client.get_raw_mempool_verbose();
+    let mempool = client.get_raw_mempool_verbose().await;
 
     assert!(mempool.is_ok());
 }
 
-#[test]
-fn test_estimate_fee() {
+//@todo prioritize transaction - see above.
+// #[runtime::test]
+// async fn test_prioritize_transaction() {
+//     let client = common::setup();
+
+//     client.prioritize_transaction(&tx_hash).await.unwrap();
+// }
+
+#[runtime::test]
+async fn test_estimate_fee() {
     let client = common::setup();
 
-    let fee = client.estimate_fee(&10);
+    let fee = client.estimate_fee(10).await;
 
     assert!(fee.is_ok());
 }
 
-#[test]
-fn test_estimate_priority() {
+#[runtime::test]
+async fn test_estimate_priority() {
     let client = common::setup();
 
-    let priority = client.estimate_priority(&10);
+    let priority = client.estimate_priority(10).await;
 
     assert!(priority.is_ok());
 }
 
-#[test]
-fn test_estimate_smart_fee() {
+#[runtime::test]
+async fn test_estimate_smart_fee() {
     let client = common::setup();
 
-    let smartfee = client.estimate_smart_fee(&10);
+    let smartfee = client.estimate_smart_fee(10).await;
 
     assert!(smartfee.is_ok());
 }
 
-#[test]
-fn test_estimate_smart_priority() {
+#[runtime::test]
+async fn test_estimate_smart_priority() {
     let client = common::setup();
 
-    let smartpriority = client.estimate_smart_priority(&10);
+    let smartpriority = client.estimate_smart_priority(10).await;
 
     assert!(smartpriority.is_ok());
 }
